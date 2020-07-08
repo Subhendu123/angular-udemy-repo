a897c63351d9e6be42e4b1d3f2d83f3b57a2218a
Subhendu123 <subhendu.sd.das@gmail.com>
2020-07-08 22:51:17 +0530

    New Proj - old Replaced

--- udemy-component/src/app/app.component.spec.ts
+++ udemy-component/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'component-preview-proj'`, () => {
+  it(`should have as title 'udemy-component'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('component-preview-proj');
+    expect(app.title).toEqual('udemy-component');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('component-preview-proj app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('udemy-component app is running!');
   });
 });
